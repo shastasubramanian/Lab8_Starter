bbc6ebe905b1e997768a4cde51872927299c6e5f
shastasubramanian <shasta.subramanian@gmail.com>
2022-11-17 14:40:12 -0800

    completed explore

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -4,35 +4,72 @@ const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
 
-test('phone number true 1', () => {
+// isPhoneNumber
+test('isPhoneNumber number true 1', () => {
   expect(functions.isPhoneNumber('(858)533-6688')).toBe(true);
 });
-
-test('phone number true 2', () => {
+test('isPhoneNumber true 2', () => {
   expect(functions.isPhoneNumber('(858)444-6688')).toBe(true);
 });
-
-test('phone number false 1', () => {
+test('isPhoneNumber false 1', () => {
   expect(functions.isPhoneNumber('1112223333')).toBe(false);
 });
-
-test('phone number false 2', () => {
+test('isPhoneNumber false 2', () => {
   expect(functions.isPhoneNumber('asdasdasdasd')).toBe(false);
 });
 
-
-test('email true 1', () => {
+// isEmail
+test('isEmail true 1', () => {
   expect(functions.isEmail('test_user@ucsd.edu')).toBe(true);
 });
-
-test('email true 2', () => {
+test('isEmail true 2', () => {
   expect(functions.isEmail('test_user@google.com')).toBe(true);
 });
-
-test('email false 1', () => {
+test('isEmail false 1', () => {
   expect(functions.isEmail('not_a_email_333')).toBe(false);
 });
-
-test('email false 2', () => {
+test('isEmail false 2', () => {
   expect(functions.isEmail('not_email_adreesssssss@emailaaaaa')).toBe(false);
+});
+
+// isStrongPassword
+test('isStrongPassword true 1', () => {
+  expect(functions.isStrongPassword('abcd12')).toBe(true);
+});
+test('isStrongPassword true 2', () => {
+  expect(functions.isStrongPassword('strongPass_')).toBe(true);
+});
+test('isStrongPassword false 1', () => {
+  expect(functions.isStrongPassword('12345')).toBe(false);
+});
+test('isStrongPassword false 2', () => {
+  expect(functions.isStrongPassword('/.*%^&*!@#$%^&*()')).toBe(false);
+});
+
+// isDate
+test('isDate true 1', () => {
+  expect(functions.isDate('02/03/2002')).toBe(true);
+});
+test('isDate true 2', () => {
+  expect(functions.isDate('2/3/2002')).toBe(true);
+});
+test('isDate false 1', () => {
+  expect(functions.isDate('12/123/2000')).toBe(false);
+});
+test('isDate false 2', () => {
+  expect(functions.isDate('1/3/40001')).toBe(false);
+});
+
+// isHexColor
+test('isHexColor true 1', () => {
+  expect(functions.isHexColor('000')).toBe(true);
+});
+test('isHexColor true 2', () => {
+  expect(functions.isHexColor('#ffffff')).toBe(true);
+});
+test('isHexColor false 1', () => {
+  expect(functions.isHexColor('01')).toBe(false);
+});
+test('isHexColor false 2', () => {
+  expect(functions.isHexColor('abc1')).toBe(false);
 });
